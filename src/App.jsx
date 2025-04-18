333b749245327ad2d24a408312fc3651d6296daf
fsociety00d4t <58801059+fsociety00d4t@users.noreply.github.com>
2025-04-18 17:32:58 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen flex items-center justify-center bg-gray-100 overflow-hidden'>
+      <h1 className='text-4xl font-bold text-purple-600'>
+        Welcome to Your Pilates Landing Page
+      </h1>
+    </div>
+  )
+}
+
+export default App
